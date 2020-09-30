e9b27c758b3ac3417d3945a8bbafc49b6bce81ab
alice2525-fox <alicekolada12@gmail.com>
2020-09-30 20:55:10 +0300

    finished tadk

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
+exports.min = function min(array) {
+    if (array && array.length > 0) {
+        const min = Math.min.apply(null, array);
+        return min;
+    } else {
+        return 0;
+    }
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array && array.length > 0) {
+        const max = Math.max.apply(null, array);
+        return max;
+    } else {
+        return 0;
+    }
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(nums) {
+    if (nums && nums.length > 0) {
+        return nums.reduce((a, b) => a + b) / nums.length;
+    } else {
+        return 0;
+    }
+};
